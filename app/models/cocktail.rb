739312e47d66ca41e1267ce9aa8c7e41a8c855a1
Rebeca Souza <rebi_britto@hotmail.com>
2018-11-16 17:16:16 +0100

    fixed picture error

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,8 +1,16 @@
 class Cocktail < ApplicationRecord
   mount_uploader :photo, PhotoUploader
-  has_many :doses, dependent: :destroy
+  has_many :doses
   has_many :ingredients, through: :doses
 
   validates :name, presence: true, uniqueness: true
+
+ before_save :assign_url
+
+ private
+
+ def assign_url
+  self.remote_photo_url = 'https://images.unsplash.com/photo-1517620430776-0ec904756579?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=631b4f98b4e594a6653ee1ae97c3a4c9&auto=format&fit=crop&w=668&q=80' if photo_url.nil?
+ end
 
 end
